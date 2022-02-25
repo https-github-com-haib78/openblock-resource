e41c5071b83d33bc8d35a78d6e9c31ae2c51dd85
ygrobot <58776239+haib78@users.noreply.github.com>
2022-02-25 19:57:49 +0800

    2-25

--- script/download.js
+++ script/download.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const configPath = path.resolve('./external-resources/config.json');
 
 const config = {};
-config.user = 'openblockcc';
+config.user = 'haib78';
 config.repo = 'external-resources';
 
 releaseDownloader.getReleaseList(`${config.user}/${config.repo}`)
